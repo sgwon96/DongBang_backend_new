1c8131737682c15a223f9099fe033b9b3630f39a
WISUNGWON <tkrhkaos12@naver.com>
2020-12-10 12:03:32 +0900

    [fix/9] 동아리 삭제 기능 구현 + shema.prisma 업데이트

--- src/api/User/signIn/signIn.js
+++ src/api/User/signIn/signIn.js
@@ -1,26 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { generateToken } from "../../../utils";
 
 const SALTROUNDS = 10;
 const prisma = new PrismaClient();
 
- export default {
-   Mutation: {
-     signIn: async (_, args) => {
-       const { email, password } = args;
-       const user = await prisma.user.findUnique({
-            where:{
-                email
-            }
-        });
-       const passwordTrue = bcrypt.compare(password, user.encryptedPassword)
-       if (passwordTrue) {
-         console.log(user);
+export default {
+  Mutation: {
+    signIn: async (_, args) => {
+      const { email, password } = args;
+      console.log(email);
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      const passwordTrue = bcrypt.compare(password, user.encryptedPassword);
+      if (passwordTrue) {
+        console.log(user);
         return generateToken(user.userId);
-       } else {
-         throw Error("Wrong email/secret combination");
-       }
-     }
-   }
- };
+      } else {
+        throw Error("Wrong email/secret combination");
+      }
+    },
+  },
+};
