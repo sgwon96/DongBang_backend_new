bd2734a34b9ccd11ddbf1222bb1cbb7942540e6d
윤승권 <gwon@yunseung-gwon-ui-MacBookPro.local>
2020-12-24 19:32:33 +0900

    [feat/16] /post/upload (파일 업로드(AWS S3) )

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
+import cors from "cors";
 
 const PORT = process.env.PORT || 4000;
 
@@ -15,6 +17,7 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", cors(), uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
