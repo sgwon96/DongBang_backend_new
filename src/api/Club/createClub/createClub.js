ed401182f7e5e8a2a524337cd52a95ad71012629
위성원 <tkrhkaos12@naver.com>
2020-12-26 02:29:36 +0900

    [feat/17] /club/question (지원서 질문 수정, 생성)

--- src/api/Club/createClub/createClub.js
+++ src/api/Club/createClub/createClub.js
@@ -4,7 +4,10 @@ const prisma = new PrismaClient();
 
 export default {
   Mutation: {
-    createClub: async (_, args) => {
+    createClub: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const userId = user.userId;
       const {
         clubName,
         clubBio,
@@ -12,7 +15,6 @@ export default {
         logoImage,
         clubImage,
         type,
-        userId,
       } = args;
       const club = await prisma.club.create({
         data: {
@@ -27,16 +29,12 @@ export default {
               userId,
             },
           },
-          members: {
-            connect: [
-              {
-                userId,
-              },
-            ],
-          },
         },
       });
+      //Todo : 동아리 생성한 아이디가 clubmemeber로 추가되어야 함.
       return club;
     },
   },
 };
+
+// 동아리를 생성하는 사람이 초기에 master가 됨.
