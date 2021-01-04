919b54b2ee45af2e1d6f4cb8640266b138b38ee3
YoonSeungGwon <zxcvb5434@likelion.org>
2021-01-04 17:10:46 +0900

    [feat]#38 model.js 재작성

--- src/api/Model/model.js
+++ src/api/Model/model.js
@@ -5,18 +5,28 @@ const prisma = new PrismaClient();
 export default {
    User: {
      clubMaster: (parent) => prisma.club.findUnique({where:{id:parent.clubId}}),
+     application: (parent) => prisma.application.findMany({where:{userId:parent.id}}),
    },
    Club: {
-    questions: (parent) => prisma.question.findMany({where:{clubId:parent.id}})
+    questions: (parent) => prisma.question.findMany({where:{clubId:parent.id}}),
+    posts: (parent) => prisma.post.findMany({where:{authorrId:parent.id}}),
+   },
+   Post: {
+     author: prisma.club.findUnique({where:{id:parent.authorId}}),
    },
    Question: {
-    choices: (parent) => prisma.choice.findMany({where:{questionId:parent.id}})
+    choices: (parent) => prisma.choice.findMany({where:{questionId:parent.id}}),
+    club: prisma.club.findUnique({where:{id:parent.clubId}}),
    },
    Choice: {
     question: (parent) => prisma.question.findUnique({where:{id:parent.questionId}})
    },
    Application: {
      club: (parent) => prisma.club.findUnique({where:{id:parent.clubId}}),
-     answers: (parent) => prisma.answer.findMany({where:{applicationId:parent.id}})
+     answers: (parent) => prisma.answer.findMany({where:{applicationId:parent.id}}),
+     user: prisma.user.findUnique({where:{id:parent.userId}}),
     },
+    Answer: {
+      application: prisma.application.findUnique({where:{id:parent.applicationId}}),
+    }
    };
