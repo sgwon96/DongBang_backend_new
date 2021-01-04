6908fb0d60c1faa1b93117423ec524d6e1446893
YoonSeungGwon <zxcvb5434@likelion.org>
2021-01-04 16:52:28 +0900

    [fix]#28 User 함수 updateUser 사용자 못찾을경우 추가 + userId->id 변경

--- src/api/User/updateUser/updateUser.js
+++ src/api/User/updateUser/updateUser.js
@@ -3,12 +3,16 @@ const prisma = new PrismaClient();
 
  export default {
    Mutation: {
-     updateUser: (_, args, { request, isAuthenticated }) => {   
+     updateUser: async (_, args, { request, isAuthenticated }) => {   
        const { id,name, studentNumber, phoneNumber, major, university } = args;
        const { user } = request;
+       const targetUser = await prisma.user.findUnique({where:{id:id}});
+       if(targetUser == null){
+         throw Error("사용자가 존재하지 않습니다.");
+       }
        if(user.id == id){
         return prisma.user.update({
-          where: { userId: user.userId },
+          where: { id: user.id },
           data: { name, studentNumber, phoneNumber, major, university }
         });
        }else{
