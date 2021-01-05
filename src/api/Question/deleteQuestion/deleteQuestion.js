51a593c15c57defb598a234896e00bdca9193c2e
YoonSeungGwon <zxcvb5434@likelion.org>
2021-01-06 01:44:37 +0900

    [fix]#40 deleteQuestion question -> Question

--- src/api/Question/deleteQuestion/deleteQuestion.js
+++ src/api/Question/deleteQuestion/deleteQuestion.js
@@ -20,7 +20,7 @@ export default {
 
         if(clubId == question.clubId){
             const question = await prisma.question.findUnique({where:{id:id}});
-            await prisma.$queryRaw(`delete from question where id = ${id}`);
+            await prisma.$queryRaw(`delete from Question where id = ${id}`);
             return question;
         } else {
             throw Error("게시글 작성자가 아닙니다.");
