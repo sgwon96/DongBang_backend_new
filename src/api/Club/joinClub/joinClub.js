62b7d48f61284fcd796ca3a85ffd0bd521ec57a3
YoonSeungGwon <zxcvb5434@likelion.org>
2021-01-11 17:41:01 +0900

    [fix]#53 joinClub 지원서 아이디를 인자로 받도록 수정

--- src/api/Club/joinClub/joinClub.js
+++ src/api/Club/joinClub/joinClub.js
@@ -7,11 +7,24 @@ export default {
     joinClub: async (_, args, { request, isAuthenticated }) => {
         isAuthenticated(request);
         const { clubId } = request.user;
-        const { userId } = args;
+        const { applicationId } = args;
+        const application = await prisma.application.findUnique({where:{id:applicationId}});
+        const userId = application.userId;
+
+        if(application == null){
+          throw Error("지원서가 존재하지 않습니다.");
+        }
+
+     
+
         const user = await prisma.user.findUnique({where:{id:userId}});
 
         if(clubId == null){
             throw Error("동아리 회장이 아닙니다.");
+        }
+
+        if(application.clubId != clubId){
+          throw Error("해당 동아리에 대한 지원서가 아닙니다.")
         }
 
         if(user == null){
