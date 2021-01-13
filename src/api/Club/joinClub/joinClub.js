f4726b5cff4c042f3dd936665db3efa909223d1b
YoonSeungGwon <zxcvb5434@likelion.org>
2021-01-13 18:32:36 +0900

    [fix]#54 joinClub 유저아이디를 인자로 받기

--- src/api/Club/joinClub/joinClub.js
+++ src/api/Club/joinClub/joinClub.js
@@ -7,26 +7,14 @@ export default {
     joinClub: async (_, args, { request, isAuthenticated }) => {
         isAuthenticated(request);
         const { clubId } = request.user;
-        const { applicationId } = args;
-        const application = await prisma.application.findUnique({where:{id:applicationId}});
-        const userId = application.userId;
-
-        if(application == null){
-          throw Error("지원서가 존재하지 않습니다.");
-        }
-
-     
+        const { userId } = args;
 
         const user = await prisma.user.findUnique({where:{id:userId}});
 
         if(clubId == null){
             throw Error("동아리 회장이 아닙니다.");
         }
-
-        if(application.clubId != clubId){
-          throw Error("해당 동아리에 대한 지원서가 아닙니다.")
-        }
-
+        
         if(user == null){
             throw Error("사용자를 찾을 수 없습니다.")
         } else {
